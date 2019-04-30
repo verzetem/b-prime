260715a6827e3a08a3b861084dd2956f14dc6edd
chris <verzetem@gmail.com>
2019-04-29 22:05:46 -0600

    added home component and made changes to nav, utilized react router dom by creating routes for nav and home components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import { DateTime } from "luxon"
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import { DateTime } from 'luxon'
 import './App.scss';
 
+import Home from './components/Home'
 import Nav from './components/Nav'
 
 function App() {
   return (
-    <div className="container-fluid">
-		  <div className="row">
-		    <Nav />
-		    <div className="col-sm-10">
 
-		    </div>
-		  </div>
-		</div>
-  );
+		<Router>
+			<div className="countainer-fluid">
+				<div className="row">
+					{/*structureCount={ this.state.structureInfo.length }*/}
+					<Route path="/" render={ (props) => <Nav /> } />
+					<Route path="/" exact component={ Home } />
+					<Switch>
+						<div className="col-sm-10">
+						{/*<Route path="/timers" exact component={ Timers } />*/}
+						{/*<Route component={ NotFound } />*/}
+						</div>
+					</Switch>
+				</div>
+			</div>
+			</Router>
+  )
 }
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 const Nav = () => {
 	return (
 		<div className="col-sm-2">
-			<div className="nav"></div>
+			<div className="nav">
+				<div className="logo">
+					<h6>BOREDOM PRIME</h6>
+				</div>
+				<div className="row">
+				  <nav className="nav flex-column">
+					  <span className="link-style"><Link className="nav-link" to="/">Home</Link></span>
+					  <span className="link-style"><Link className="nav-link" to="/">Timers</Link></span>
+					  <a className="nav-link disabled" href="#">Dscan</a>
+					</nav>
+					</div>
+			</div>
 		</div>
 		)
 }
