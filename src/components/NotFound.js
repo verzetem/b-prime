434bc1da0d4a25f036b1eda26b5b7d7ccf095c7c
chris <verzetem@gmail.com>
2019-04-30 22:25:20 -0600

    added redirect to notfound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 
 const NotFound = () => {
 	return (
 		<div className="home">
 			<h1>Page Not Found.</h1>
 			Return <Link className="return-home" to="/">Home</Link>?
+
+			<Redirect to="/" />
 		</div>
 		)
 }
