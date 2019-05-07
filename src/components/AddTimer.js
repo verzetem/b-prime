949e2be89222e98028267a1d1b49cc818e0f9898
chris <verzetem@gmail.com>
2019-05-07 13:05:58 -0600

    used prettier, locked top table row and made timer table scrollable

--- src/components/AddTimer.js
+++ src/components/AddTimer.js
@@ -1,27 +1,86 @@
-import React from 'react'
+import React from "react";
 
-const AddTimer = ({ onSubmit, notification, resetInput, newStructure, nameListen, timeListen, locationListen, daysListen, hoursListen, minutesListen, secondsListen }) => {
+const AddTimer = ({
+	onSubmit,
+	notification,
+	resetInput,
+	newStructure,
+	nameListen,
+	timeListen,
+	locationListen,
+	daysListen,
+	hoursListen,
+	minutesListen,
+	secondsListen
+}) => {
 	return (
 		<div className="home">
 			<h1>Add Timer</h1>
 
-
 			<form>
-			  <div className="form-group">
-			    <input type="text" className="form-control input-one" placeholder="Structure Name" onChange={(e) => nameListen(e)} value={newStructure.newName} />
-			    <input type="text" className="form-control input-one" placeholder="Location" onChange={(e) => locationListen(e)} value={newStructure.newLocation} />
-			  </div>
+				<div className="form-group">
+					<input
+						type="text"
+						className="form-control input-one"
+						placeholder="Structure Name"
+						onChange={e => nameListen(e)}
+						value={newStructure.newName}
+					/>
+					<input
+						type="text"
+						className="form-control input-one"
+						placeholder="Location"
+						onChange={e => locationListen(e)}
+						value={newStructure.newLocation}
+					/>
+				</div>
 			</form>
 			<form className="form-inline">
-				<input type="number" className="form-control input-two" placeholder="Days" onChange={(e) => daysListen(e) } value={newStructure.newTime.days} />
-		    <input type="number" className="form-control input-two" placeholder="Hours" onChange={(e) => hoursListen(e) } value={newStructure.newTime.hours} />
-		    <input type="number" className="form-control input-two" placeholder="Minutes" onChange={(e) => minutesListen(e) } value={newStructure.newTime.minutes} />
-		    <input type="number" className="form-control input-two" placeholder="Seconds" onChange={(e) => secondsListen(e) } value={newStructure.newTime.seconds} />
+				<input
+					type="number"
+					className="form-control input-two"
+					placeholder="Days"
+					onChange={e => daysListen(e)}
+					value={newStructure.newTime.days}
+				/>
+				<input
+					type="number"
+					className="form-control input-two"
+					placeholder="Hours"
+					onChange={e => hoursListen(e)}
+					value={newStructure.newTime.hours}
+				/>
+				<input
+					type="number"
+					className="form-control input-two"
+					placeholder="Minutes"
+					onChange={e => minutesListen(e)}
+					value={newStructure.newTime.minutes}
+				/>
+				<input
+					type="number"
+					className="form-control input-two"
+					placeholder="Seconds"
+					onChange={e => secondsListen(e)}
+					value={newStructure.newTime.seconds}
+				/>
 			</form>
-			<button type="button" className="btn btn-primary" onClick={ (e) => onSubmit(e) }>Submit</button>
-			<button type="button" className="btn btn-warning" onClick={ (e) => resetInput(e) }>Reset</button>
+			<button
+				type="button"
+				className="btn btn-primary"
+				onClick={e => onSubmit(e)}
+			>
+				Submit
+			</button>
+			<button
+				type="button"
+				className="btn btn-warning"
+				onClick={e => resetInput(e)}
+			>
+				Reset
+			</button>
 		</div>
-		)
-}
+	);
+};
 
-export default AddTimer
+export default AddTimer;
--- src/components/Updates.js
+++ src/components/Updates.js
@@ -1,39 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const Updates = () => {
 	return (
 		<div>
-			  <button className="btn btn-secondary" id="updates" type="button" data-toggle="collapse" data-target="#updatesCollapse" aria-expanded="false" aria-controls="updatesCollapse">
-			    Updates
-			  </button>
+			<button
+				className="btn btn-secondary"
+				id="updates"
+				type="button"
+				data-toggle="collapse"
+				data-target="#updatesCollapse"
+				aria-expanded="false"
+				aria-controls="updatesCollapse"
+			>
+				Updates
+			</button>
 			<div className="collapse" id="updatesCollapse">
-
-
-
-
-				<div className="card text-white bg-dark mb-3" style={{ height: "11em" }} >
-				  <div className="card-body update-card">
-				   	<p className="card-text">
-				    	Apr 30 - MVP complete.
-				    	<hr/>
-				    	May 3 - Added local and EVE times, cleaned up CSS.
-				    	{/*<div className="arrow bounce">
+				<div
+					className="card text-white bg-dark mb-3"
+					style={{ height: "11em" }}
+				>
+					<div className="card-body update-card">
+						<p className="card-text">
+							May 7 - Locked first timer row/made timer table scrollable
+							<hr />
+							May 6 - Dev build Deployed 🎉🎉🎉🎉🎉
+							<hr />
+							May 5 - Cleaned up CSS, ready to deploy.
+							{/*<div className="arrow bounce">
 							  <a className="fas fa-angle-down fa-2x updates-dn-arrow" href="#"></a>
 							</div>*/}
-				    	<hr/>
-				    	May 5 - Cleaned up CSS, ready to deploy.
-				    	<hr/>
-				    	May 6 - Dev build Deployed 🎉🎉🎉🎉🎉
-				   	</p>
-				  </div>
+							<hr />
+							May 3 - Added local and EVE times, cleaned up CSS.
+							<hr />
+							Apr 30 - MVP complete.
+						</p>
+					</div>
 				</div>
-
-
-
-
 			</div>
 		</div>
-		)
-}
+	);
+};
 
-export default Updates
+export default Updates;
